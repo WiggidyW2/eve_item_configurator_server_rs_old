42cd7b89d83a7014455ed765edbfa4155019d413
Wiggidy <rlr0017@auburn.edu>
2023-06-21 15:43:30 -0500

    added support for GRPC-Web

--- src/lib.rs
+++ src/lib.rs
@@ -15,37 +15,30 @@ pub use error::Error;
 
 mod accessors;
 pub use accessors::{
-    Accessor,
-    TypeIdGetter,
-    NameGetter,
-    MarketGroupGetter,
-    GroupGetter,
-    CategoryGetter,
-    JsonGetter,
-    JsonSetter,
-    ItemGetter,
-    ItemSetter,
-    CharacterGetter,
-    CharacterSetter,
+    Accessor, CategoryGetter, CharacterGetter, CharacterSetter, GroupGetter, ItemGetter,
+    ItemSetter, JsonGetter, JsonSetter, MarketGroupGetter, NameGetter, TypeIdGetter,
 };
 
 // re-export for convenience to accessor implementors
 pub use tonic::async_trait;
+
+use tonic_web;
 
 pub async fn serve(
     accessor: impl Accessor,
     client_id: String,
     address: std::net::SocketAddr,
     server: &mut tonic::transport::Server,
+    http1: bool,
 ) -> Result<(), Error> {
-    server
-        .add_service(pb::server::ItemConfiguratorServer::new(
-            service::Service {
-                accessor: accessors::AccessorWrapper(accessor),
-                validator: validator::Validator::new(client_id).await?,
-            }
-        ))
-        .serve(address)
-        .await?;
+    let service = pb::server::ItemConfiguratorServer::new(service::Service {
+        accessor: accessors::AccessorWrapper(accessor),
+        validator: validator::Validator::new(client_id).await?,
+    });
+    let router = match http1 {
+        true => server.add_service(tonic_web::enable(service)),
+        false => server.add_service(service),
+    };
+    router.serve(address).await?;
     Ok(())
 }
